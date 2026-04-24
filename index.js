04f2f6e69359fba58c3bb10b1f8d219f79cb8f8e
mairie-mezieres <fabrice.auffret45000@gmail.com>
2026-04-24 07:26:10 +0200

    fix: CORS PATCH method + vote validation + sondage results endpoint

--- index.js
+++ index.js
@@ -312,7 +312,7 @@ function compactSeenMap(mapObj = {}, keepKeys = []) {
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin",  "*");
 res.header("Access-Control-Allow-Headers", "Content-Type, x-admin-token, x-device-id");
-  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
   next();
 });
 app.options("*", (req, res) => res.sendStatus(200));
