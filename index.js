4d4e2d83dd0d3211155105a29a4ffc00139da334
mairie-mezieres <fabrice.auffret45000@gmail.com>
2026-04-28 19:07:01 +0200

    fix: ajouter PUT dans Access-Control-Allow-Methods (CORS)

--- index.js
+++ index.js
@@ -404,7 +404,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
   }
   res.header("Access-Control-Allow-Headers", "Content-Type, x-admin-token, x-device-id");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
   next();
 });
 app.options("*", (req, res) => res.sendStatus(200));
